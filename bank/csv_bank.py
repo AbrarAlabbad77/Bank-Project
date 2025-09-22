b30978d0d34723eb6b05e79db9e44fe42bb02daf
“abraralabbad <AbrarAlabbad192@outlook.com>
2025-09-22 23:58:16 +0300

    header function

--- bank/csv_bank.py
+++ bank/csv_bank.py
@@ -2,22 +2,37 @@ import csv
 from bank.customer import Customer
 # from .customer import Customer
 
-# def header(filename='bank.csv'):
-#     with open(filename, 'a', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(['First Name', 'Second Name','Password', 'Balance Saving Account', 'Balance Checking Account'])
+
+def heading():
+    with open('bank.csv', 'r', newline='') as file:
+        reader = list(csv.reader(file))
+        if len(reader) == 3:   # empty file
+            next_id = 3
+        else:
+            next_id = len(reader) 
+        
+    with open('bank.csv', 'a', newline='') as file:
+        writer = csv.writer(file)   
+        if len(reader) == 3:
+            writer.writerow(["ID", "FirstName", "LastName", "Password", "BalanceSaving", "BalanceChecking"])
+    
+
+# Reading from a CSV file
+def reading():
+    with open('bank.csv', 'r', newline='') as file:
+        # reader = csv.reader(file)
+        reader = list(csv.reader(file))
+        for row in reader:
+            print(row[0])
+
 
 def addNewCustomer(Customer):
     idd = 1000 
+        
+        # reader = list(csv.reader(file))
     with open('bank.csv', 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([Customer.firstName, Customer.secondName,Customer.password,
                          Customer.balancSavingAccount, Customer.balancCheckingAccount])
 
 
-# Reading from a CSV file
-def reading():
-    with open('bank.csv', 'r', newline='') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row[0])
